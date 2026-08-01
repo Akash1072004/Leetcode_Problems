d86bd575c7a766904f1f2255966f27921834b31d
Akash Kumar <akashkumar1072004@gmail.com>
2026-08-01 10:54:14 +0530

    Time: 174 ms (5.37%), Space: 9.5 MB (96.48%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int f(vector<int>& nums, int i, int j, bool turn) {
+        if(i > j) return 0;
+        int ans;
+        if(!turn) {
+            ans = -1e9;
+            ans = max(ans, nums[i]+f(nums, i+1, j, true));
+            ans = max(ans, nums[j]+f(nums, i, j-1, true));
+        }
+        else {
+            ans = 1e9;
+            ans = min(ans, -nums[i]+f(nums, i+1, j, false));
+            ans = min(ans, -nums[j]+f(nums, i, j-1, false));
+        }
+
+        return ans;
+    }
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n = nums.size();
+        
+        int ans = f(nums, 0, n-1, false);
+
+        return (ans >= 0) ? true : false;
+
+    }
+};
