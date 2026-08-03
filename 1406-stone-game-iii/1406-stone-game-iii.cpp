52822ba297db6e922289e05a61b35d16c1bfc161
Akash Kumar <akashkumar1072004@gmail.com>
2026-08-03 09:54:22 +0530

    Time: 77 ms (18.03%), Space: 136.9 MB (60.31%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int dp[50001][2];
+    int f(vector<int>& stoneValue, int i, bool turn, int n) {
+        if(i >= n) return 0;
+        if(dp[i][turn] != -1) return dp[i][turn];
+        int ans = 0;
+        if(turn) {
+            ans = -1e9;
+            ans = max(ans, stoneValue[i]+f(stoneValue, i+1, false, n));
+            if(i+1 < n) ans = max(ans, stoneValue[i]+stoneValue[i+1]+f(stoneValue, i+2, false, n));
+            if(i+2 < n) ans = max(ans, stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]+f(stoneValue, i+3, false, n));
+        }
+        else {
+            ans = 1e9;
+            ans = min(ans, -stoneValue[i]+f(stoneValue, i+1, true, n));
+            if(i+1 < n) ans = min(ans, -(stoneValue[i]+stoneValue[i+1])+f(stoneValue, i+2, true, n));
+            if(i+2 < n) ans = min(ans, -(stoneValue[i]+stoneValue[i+1]+stoneValue[i+2])+f(stoneValue, i+3, true, n));
+        }
+        return dp[i][turn] = ans;
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        
+        int n = stoneValue.size();
+        memset(dp, -1, sizeof dp);
+        int ans = f(stoneValue, 0, true, n);
+        cout << ans << " ";
+        if(ans < 0) return "Bob";
+        else if(ans > 0) return "Alice";
+        else return "Tie";
+
+    }
+};
